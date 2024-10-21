6dfc7a79316e0c6e058171037133464708fa57f2
chiayinin <chiayinin@gmail.com>
2024-10-21 17:55:44 +0800

    feat: footer RWD done.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,9 +6,9 @@ import Logo from "@assets/images/logo_white.svg";
 
 export const Footer = () => {
   return(<>
-  <footer className="bg-neutral-bg text-neutral-40 pt-20 pb-[120px] px-4">
+  <footer className="bg-neutral-bg text-neutral-40 px-3 py-20 md:pt-20 md:pb-[120px] md:px-4">
     <div className="container max-w-[1296px]">
-      <div className="flex justify-between items-start mb-24 ">
+      <div className="flex justify-between items-start flex-col gap-10 mb-20  md:mb-24 md:flex-row">
         {/* Logo */}
         <div>
           <Link to={'/'}  className="block mb-10 w-[196px]"><img src={ Logo } alt="享樂酒店" /></Link>
@@ -22,8 +22,8 @@ export const Footer = () => {
           </ul>
         </div>
         {/* 聯絡資訊 */}
-        <div className="flex justify-end gap-x-20">
-          <ul className="flex flex-col gap-y-10">
+        <div className="flex flex-col gap-4 md:flex-row md:justify-end md:gap-x-20">
+          <ul className="flex flex-col gap-4 md:gap-y-10">
             <li title="享樂酒店 TEL">
               <p className="text-title mb-2">TEL</p>
               <a className="text-body" href="tel:+886-7-1234567">+886-7-1234567</a>
@@ -33,7 +33,7 @@ export const Footer = () => {
               <a className="text-body" href="tel:+886-7-1234567">+886-7-1234567</a>
             </li>
           </ul>
-          <ul className="flex flex-col gap-y-10">
+          <ul className="flex flex-col gap-4 md:gap-y-10">
             <li title="享樂酒店 MAIL">
               <p className="text-title mb-2">MAIL</p>
               <a className="text-body" href="mailto:elh@hexschool.com">elh@hexschool.com</a>
@@ -46,7 +46,7 @@ export const Footer = () => {
         </div>
       </div>
       {/* 其他資訊 */}
-      <div className="flex justify-between items-center text-body">
+      <div className="flex justify-between text-body flex-col gap-4 md:flex-row md:items-center">
         <address title="806023 台灣高雄市新興區六角路123號">806023 台灣高雄市新興區六角路123號</address>
         <p>&copy; 享樂酒店 2023 All Rights Reserved.</p>
       </div>
