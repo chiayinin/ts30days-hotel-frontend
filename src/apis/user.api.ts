f576afa2694daab599a95f7e846c117799ee9f6d
chiayinin <chiayinin@gmail.com>
2024-10-26 19:06:55 +0800

    feat: user api done.

--- src/apis/user.api.ts
+++ src/apis/user.api.ts
@@ -1,3 +1,19 @@
 import { redirect } from 'react-router-dom';
 import { fetchData, getFromStorage, KEY_TOKEN } from '@core';
 import { User } from '@types';
+
+export const signUp = async (params: User) => fetchData<User>('post', '/user/signup', params);
+
+export const login = async (params: { email: string, password: string }) => fetchData<User>('post', '/user/login', params);
+
+export const getUser = async (token: string) => fetchData<User | null>('get', '/user', undefined, token);
+
+export const loginGuard = async () => {
+  const token = getFromStorage(KEY_TOKEN, 'COOKIE');
+
+  if(token) {
+    const user = await getUser(token);
+    if(user) return redirect('/');
+  };
+  return null;
+};
