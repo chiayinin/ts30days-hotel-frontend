3dc3d8e4ef7108196c778d02376a31b5d9836a23
chiayinin <chiayinin@gmail.com>
2024-10-26 18:43:06 +0800

    feat: http management done.

--- src/environments/environment.ts
+++
@@ -1,1 +0,0 @@
-export const BASE_URL = 'https://ts30days-hotel-backend.onrender.com/api/v1';
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), svgr(), tsconfigPaths()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env,
+          changeOrigin: true,
+        }
+      }
+    }
+  }
 });
