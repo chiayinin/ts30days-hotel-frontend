b21b0170632565808d13dd6781495021a4f8491b
chiayinin <chiayinin@gmail.com>
2024-12-06 21:02:12 +0800

    page: home-page banner done.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import { PrimeReactProvider } from 'primereact/api';
 
 // style
 import Tailwind from 'primereact/passthrough/tailwind';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
 
 const App = () => {
   return (
--- src/app-routing.tsx
+++ src/app-routing.tsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import Home from "./pages/Home/Home";
+
 // 頁面元件
 import Layout from "./pages/Layout/Layout";
-
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />
+      },
+    ]
   }
 ])
