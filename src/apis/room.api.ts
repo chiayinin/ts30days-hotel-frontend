58056e73a4f212e59bc1b033f2064b88cf1d56ee
chiayinin <chiayinin@gmail.com>
2025-03-18 11:26:38 +0800

    feat: room fake data done.

--- src/apis/room.api.ts
+++ src/apis/room.api.ts
@@ -1,4 +1,4 @@
 import { fetchData } from "@core";
 import { Room } from '@types';
 
-export const getRoomsData = async () => fetchData<Room[]>('get', 'rooms').catch(() => [] as Room[]);;
+export const getRoomsData = async () => fetchData<Room[]>('get', 'rooms').catch(() => [] as Room[]);
