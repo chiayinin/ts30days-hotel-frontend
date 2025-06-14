161d415697a118d0c1026d93484efa1be5e7ce23
chiayinin <chiayinin@gmail.com>
2025-06-14 20:12:14 +0800

    bulid: done.

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    base: '/',
+    // base: '/',
     plugins: [react(), svgr(), tsconfigPaths()],
     server: {
       proxy: {
