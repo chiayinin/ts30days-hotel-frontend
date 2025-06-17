9728915031bbb60286632e4c285e6b03d7140824
chiayinin <chiayinin@gmail.com>
2025-06-17 21:58:52 +0800

    deploy: fix error

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useState, useEffect, useContext } from 'react';
 
 import Logo from "@assets/images/logo_white.svg";
