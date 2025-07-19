55c975d16123566b51537b449e92d51841779151
chiayinin <chiayinin@gmail.com>
2025-07-19 17:13:06 +0800

    deploy: change

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react(), svgr(), tsconfigPaths()],
     // base: "/ts30days-hotel-frontend/", // 設定為 GitHub Pages 專案路徑
-    base: "/", // 設定為 Vercel 專案路徑
+    // base: "/", // 設定為 Vercel 專案路徑
     server: {
       proxy: {
         '/api': {
